b5e8fc0b5197329c7e00cb5d5798f4d6f970f7ef
☈king <rkingithub@sharpsaw.org>
2013-05-31 20:02:29 +0400

    Case insensitive by default

--- lib/bbl.rb
+++ lib/bbl.rb
@@ -4,7 +4,7 @@ module Bbl
       validate argv
       levels_above = '../../..'
       path = File.expand_path __FILE__ + levels_above + '/akjv.txt'
-      cmd = ['grep'] + argv + [path]
+      cmd = ['grep', '-i'] + argv + [path]
       system *cmd
     end
 
@@ -13,7 +13,7 @@ module Bbl
     end
     def show_help
       puts <<EOT
-\e[35;1mThis is just a wrapper around grep ____ akjv.txt\e[0m
+\e[35;1mThis is just a wrapper around grep -i ____ akjv.txt\e[0m
 (so, for now, the help is: man grep ).
 If you want to see the whole text, you can do:
   \e[35mbbl . | less\e[0m
